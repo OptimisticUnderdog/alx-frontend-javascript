e2a69ae26384f9d8c295261bb4dcb74233d9e625
OptimisticUnderdog <mabunda.wealth@gmail.com>
2023-06-26 11:38:06 +0000

    0. Const or let?

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,5 +1,5 @@
 export function taskFirst() {
-	const task = 'I prefer const when I can.':
+	const task = 'I prefer const when I can.';
 	return task;
 }
 
@@ -9,7 +9,7 @@ export function getLast() {
 
 export function taskNext() {
 	let combination = 'But sometimes let';
-	combination += getlast();
+	combination += getLast();
 
 	return combination;
 }
