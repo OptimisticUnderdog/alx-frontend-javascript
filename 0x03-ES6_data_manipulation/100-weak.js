a9712308f8b01a21b65ec6de6708031b46aff5a8
OptimisticUnderdog <mabunda.wealth@gmail.com>
2023-07-05 07:34:29 +0000

    correction

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,16 +1,13 @@
 export const weakMap = new WeakMap();
 
-export function queryAPI(endpoint) {
-  let called = weakMap.get(endpoint) || 0;
-
-  called += 1;
-
-  weakMap.set(endpoint, called);
-
-  if (called >= 5) {
-    throw Error('Endpoint load is high');
+export const queryAPI = (endpoint) => {
+  if (weakMap.has(endpoint)) {
+    const endpointData = weakMap.get(endpoint);
+    if (endpointData >= 4) {
+      throw new Error('Endpoint load is high');
+    }
+    weakMap.set(endpoint, (endpointData + 1));
+  } else {
+    weakMap.set(endpoint, 1);
   }
-
-  return called;
-}
-
+};
